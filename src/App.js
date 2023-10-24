4527e92db774a9539faafb2ed6c44e8e2b71a86b
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-10-24 20:38:44 +0000

    Done with Togoggle Task

--- src/App.js
+++ src/App.js
@@ -33,17 +33,32 @@ function App() {
 
   // delete Task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== 
+    id))
+  }
+
+  // TOGGLE REMINDER
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+      task.id === id ? {...task, reminder:
+      !task.reminder} : task
+      )
+    )
   }
 
   return (
     <div className="container">
       <Header/>
-      <Tasks  tasks={tasks} onDelete=
-      {deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App
 
